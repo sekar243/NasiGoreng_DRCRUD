b00f617bcce403adfafa1c4ded4d824767d594da
Sekar243 <kinasekin6602@gmail.com>
2026-04-10 13:12:05 +0700

    menampilkan data dengan SqlDataReader

--- CRUDMahasiswaADO/Form1.cs
+++ CRUDMahasiswaADO/Form1.cs
@@ -43,5 +43,47 @@ namespace CRUDMahasiswaADO
 
         }
 
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (conn.State == System.Data.ConnectionState.Closed)
+                {
+                    conn.Open();
+                }
+                dataGridView1.Rows.Clear();
+                dataGridView1.Columns.Clear();
+
+                dataGridView1.Columns.Add("NIM", "NIM");
+                dataGridView1.Columns.Add("Nama", "Nama");
+                dataGridView1.Columns.Add("JenisKelamin", "JenisKelamin");
+                dataGridView1.Columns.Add("TanggalLahir", "TanggalLahir");
+                dataGridView1.Columns.Add("Alamat", "Alamat");
+                dataGridView1.Columns.Add("KodeProdi", "KodeProdi");
+
+                string query = "SELECT * FROM Mahasiswa";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    dataGridView1.Rows.Add(
+                        reader["NIM"].ToString(),
+                        reader["Nama"].ToString(),
+                        reader["JenisKelamin"].ToString(),
+                        Convert.ToString(reader["TanggalLahir"]).ToString(),
+                        reader["Alamat"].ToString(),
+                        reader["KodeProdi"].ToString()
+                    );
+                }
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Gagal menampilkan data: " + ex.Message);
+            }
+        
+    }
     }
 }
