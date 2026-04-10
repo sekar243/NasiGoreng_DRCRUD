b8c81296b2b0d80d451d4cc51af44e02673cf7a5
Sekar243 <kinasekin6602@gmail.com>
2026-04-10 13:05:40 +0700

    menambahkan variabel koneksi

--- CRUDMahasiswaADO/Form1.cs
+++ CRUDMahasiswaADO/Form1.cs
@@ -11,8 +11,12 @@ using System.Windows.Forms;
 
 namespace CRUDMahasiswaADO
 {
+
     public partial class Form1: Form
     {
+        private readonly SqlConnection conn;
+        private readonly string connctionString =
+            "Data Source=SKRKINASIH\\SEKAR;Initial Catalog=DBAkademikADO;Integrated Security=True";
         public Form1()
         {
             InitializeComponent();
