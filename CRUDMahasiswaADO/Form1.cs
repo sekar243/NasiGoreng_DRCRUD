da3065647f905d194087385b40fc9a9dd8fdea7d
Sekar243 <kinasekin6602@gmail.com>
2026-04-10 13:10:04 +0700

    membuat method koneksi database

--- CRUDMahasiswaADO/Form1.cs
+++ CRUDMahasiswaADO/Form1.cs
@@ -23,9 +23,25 @@ namespace CRUDMahasiswaADO
             conn = new SqlConnection(connctionString);
         }
 
+        private void ConnectDatabase()
+        {
+            try
+            {
+                if (conn.State == System.Data.ConnectionState.Closed)
+                {
+                    conn.Open();
+                }
+                MessageBox.Show("Koneksi berhasil");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Koneksi gagal: " + ex.Message);
+            }
+        }
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
+
     }
 }
