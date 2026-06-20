d0f37ebfee25d21ca869ad6a8e1fdfc74b83cfc6
Sekar243 <kinasekin6602@gmail.com>
2026-06-20 21:33:47 +0700

    Menambahkan method GetMhs untuk mengambil semua data mahasiswa

--- CRUDMahasiswaADO/DAL.cs
+++ CRUDMahasiswaADO/DAL.cs
@@ -39,6 +39,23 @@ namespace CRUDMahasiswaADO
             cmd.ExecuteNonQuery();
             return Convert.ToInt32(outputParam.Value);
         }
+        public DataTable GetMhs()
+        {
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+
+            SqlCommand cmd = new SqlCommand("sp_GetMahasiswa", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            da = new SqlDataAdapter(cmd);
+            dtMahasiswa = new DataTable();
+            da.Fill(dtMahasiswa);
+
+            return dtMahasiswa;
+        }
+
 
     }
 }
