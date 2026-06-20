fc5361deb95f281b553525ab8ce863e1b120cd49
Sekar243 <kinasekin6602@gmail.com>
2026-06-20 21:55:31 +0700

    Menambhakan method InsertLog untuk pencatatan pesan log via stored procedure

--- CRUDMahasiswaADO/DAL.cs
+++ CRUDMahasiswaADO/DAL.cs
@@ -160,6 +160,18 @@ namespace CRUDMahasiswaADO
 
             return dtMahasiswa;
         }
+        public void InsertLog(string message)
+        {
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
 
+            SqlCommand cmd = new SqlCommand("sp_LogMessage", conn);
+
+            cmd.Parameters.AddWithValue("psn", message);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.ExecuteNonQuery();
+        }
     }
 }
