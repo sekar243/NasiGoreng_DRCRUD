d470489d3175e32630301e2cf9446dd9f5dd5ed0
Sekar243 <kinasekin6602@gmail.com>
2026-06-20 21:59:42 +0700

    menambahkan method getDataChartByTahun untuk filter data grafik berdasarkan tahun

--- CRUDMahasiswaADO/DAL.cs
+++ CRUDMahasiswaADO/DAL.cs
@@ -220,5 +220,20 @@ namespace CRUDMahasiswaADO
             da.Fill(dtMahasiswa);
             return dtMahasiswa;
         }
+        public DataTable getDataChartByTahun(DateTime thMasuk)
+        {
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+
+            SqlCommand cmd = new SqlCommand("sp_DashBoardByTahun", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@inTglMsuk", thMasuk.Year);
+            da = new SqlDataAdapter(cmd);
+            dtMahasiswa = new DataTable();
+            da.Fill(dtMahasiswa);
+            return dtMahasiswa;
+        }
     }
 }
