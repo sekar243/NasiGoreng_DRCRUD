e8eff045eeaaff0eb8f4c1ead21683c63057d1b0
Sekar243 <kinasekin6602@gmail.com>
2026-06-20 21:56:22 +0700

    menambhakan method getProdi untuk mengambil daftar nama program studi

--- CRUDMahasiswaADO/DAL.cs
+++ CRUDMahasiswaADO/DAL.cs
@@ -173,5 +173,20 @@ namespace CRUDMahasiswaADO
             cmd.CommandType = CommandType.StoredProcedure;
             cmd.ExecuteNonQuery();
         }
+        public DataTable getProdi()
+        {
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+
+            SqlCommand cmd = new SqlCommand("select namaprodi from prodi", conn);
+            cmd.CommandType = CommandType.Text;
+            dtProdi = new DataTable();
+            da = new SqlDataAdapter(cmd);
+            da.Fill(dtProdi);
+
+            return dtProdi;
+        }
     }
 }
