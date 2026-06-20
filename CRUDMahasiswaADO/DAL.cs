49c08ad7a6a392e5ae6d72b246c02e38ddbe29e9
Sekar243 <kinasekin6602@gmail.com>
2026-06-20 21:44:20 +0700

    Pemetaan parametes pNIM dan eksekusi stored procedure sp_DeleteMahasiswa

--- CRUDMahasiswaADO/DAL.cs
+++ CRUDMahasiswaADO/DAL.cs
@@ -111,6 +111,11 @@ namespace CRUDMahasiswaADO
             {
                 conn.Open();
             }
+            SqlCommand cmd = new SqlCommand("sp_DeleteMahasiswa", conn);
+            cmd.Parameters.AddWithValue("pNIM", nim);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.ExecuteNonQuery();
         }
     }
 }
