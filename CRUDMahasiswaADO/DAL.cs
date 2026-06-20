927d22fb51295567014373d9f90e45844a297123
Sekar243 <kinasekin6602@gmail.com>
2026-06-20 22:03:59 +0700

    menambahkan method GetLoacalIPAddress untukmendapatkan IPv4 local

--- CRUDMahasiswaADO/DAL.cs
+++ CRUDMahasiswaADO/DAL.cs
@@ -235,5 +235,26 @@ namespace CRUDMahasiswaADO
             da.Fill(dtMahasiswa);
             return dtMahasiswa;
         }
+        public static string GetLoacalIPAddress()
+        {
+            string localIP = string.Empty;
+            try
+            {
+                var host = System.Net.Dns.GetHostEntry(System.Net.Dns.GetHostName());
+                foreach (var ip in host.AddressList)
+                {
+                    if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                    {
+                        localIP = ip.ToString();
+                        break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error getting local IP address: " + ex.Message);
+            }
+            return localIP;
+        }
     }
 }
